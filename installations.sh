dd49b6dd2e354522fac27cb37d0a5133c9750bff
Phaneendra Chidella <57545069+therealchidella@users.noreply.github.com>
2020-08-01 16:22:23 -0400

    Added first file

---
+++ installations.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Jenkins Installation
+
+sudo yum install java-1.8.0-openjdk-devel
+
+curl --silent --location http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo | sudo tee /etc/yum.repos.d/jenkins.repo
+
+sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
+
+sudo yum install jenkins
+
+sudo systemctl start jenkins
+
+sudo systemctl enable jenkins
+
+# Docker Installation
+
+curl -fsSL https://get.docker.com -o get-docker.sh
+
+sh get-docker.sh
+
+# Ansible Installation
+
+sudo yum install ansible -y
+
