00417f9b80d10d7c70c36ec73ef919e8002d70ed
Phaneendra Chidella <57545069+therealchidella@users.noreply.github.com>
2020-08-01 20:02:54 -0400

    WOrker and master files

---
+++ installations_master.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Vim Installation
+
+sudo yum install vim-enhanced -y
+# Jenkins Installation
+
+sudo yum install java-1.8.0-openjdk-devel -y
+
+curl --silent --location http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo | sudo tee /etc/yum.repos.d/jenkins.repo
+
+sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
+
+sudo yum install jenkins -y
+
+sudo systemctl start jenkins
+
+sudo systemctl enable jenkins
+
+# Docker Installation
+
+curl -fsSL https://get.docker.com -o get-docker.sh
+
+sh get-docker.sh
+
+# Ansible Installation
+
+sudo yum install epel-release -y
+
+sudo yum install ansible -y
+
---
+++ installations_worker.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Vim Installation
+
+sudo yum install vim-enhanced -y
+# Jenkins Installation
+
+sudo yum install java-1.8.0-openjdk-devel -y
+
